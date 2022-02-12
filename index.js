c8fe4f496d47a91aff64c385268eb32091765cea
Akash <98978949+Akashh26@users.noreply.github.com>
2022-02-12 10:40:30 +0530

    uploaded

---
+++ index.js
@@ -0,0 +1,25 @@
+var randomnumber1;
+randomnumber1=Math.floor(((Math.random()*6)+1));
+var randomdiceimage= "dice"+randomnumber1+".png";
+var randomimagesource="images/"+randomdiceimage;
+
+document.querySelectorAll("img")[0].setAttribute("src",randomimagesource);
+
+var randomnumber2=Math.floor(Math.random()*6)+1;
+var randomdiceimage2="dice"+randomnumber2+".png";
+var randomimagesource2="images/"+randomdiceimage2;
+document.querySelectorAll("img")[1].setAttribute("src",randomimagesource2);
+
+if(randomnumber1>randomnumber2){
+
+    document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+
+}
+
+else if(randomnumber1<randomnumber2){
+    document.querySelector("h1").innerHTML="🚩Player 2 Wins!";
+
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
